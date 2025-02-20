95b06105ebf5f12fd976ebabdc49c96a651e3027
laura-diaz-dvt <laura.diaz@devoteam.com>
2025-02-20 11:59:56 +0000

    blabla

--- generate_key.py
+++ generate_key.py
@@ -2,12 +2,12 @@ import hmac
 import hashlib
 import os
 
-secret_key = bytes(os.getenv("ACTIONHUB_SECRET"), "utf-8")
+secret_key = bytes("a79a0a190a0d79b39a34ef8ce55d1e742e29729dce7cbcb2845c975f062c6fb7", "utf-8")
 random_key = os.urandom(32).hex()
 
 hmac_object = hmac.new(secret_key, random_key.encode(), hashlib.sha512).hexdigest()
 
 
 print("Generated authorization key:")
-print(secret_key + "/" + hmac_object)
+print("a79a0a190a0d79b39a34ef8ce55d1e742e29729dce7cbcb2845c975f062c6fb7" + "/" + hmac_object)
 print("Copy this key into Looker Action as authorization key.")
